b239cf012c3d79d152344cf837825e0987cdc472
Zmiter <ozmiter@gmail.com>
2021-11-03 11:45:03 +0300

    feat: add redux store template

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import {store} from "./store";
+import {Provider} from "react-redux";
 
 function App() {
   return (
-    <div className="App">
-test
-    </div>
+      <Provider store={store}>
+          <div className="App">
+              test
+          </div>
+      </Provider>
+
   );
 }
 
