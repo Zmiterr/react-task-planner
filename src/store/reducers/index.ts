2ff7a163e4a424991c98048df6b0aff4e01f5c36
Zmiter <ozmiter@gmail.com>
2021-11-03 11:50:03 +0300

    feat: add type AppDispatch

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,7 +1,9 @@
 import {combineReducers} from "redux";
+import {store} from "../index";
 
 export const rootReducer = combineReducers({
 
 })
 
 export type RootState = ReturnType<typeof rootReducer>
+export type AppDispatch = ReturnType<typeof store.dispatch>
