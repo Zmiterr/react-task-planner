b1af519a5efa7f86ec5434c8772a4436b60acb60
Zmiter <ozmiter@gmail.com>
2021-11-03 22:57:59 +0300

    fix: change export AppDispatch

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -7,4 +7,5 @@ export const rootReducer = combineReducers({
 })
 
 export type RootState = ReturnType<typeof rootReducer>
-export type AppDispatch = ReturnType<typeof store.dispatch>
+//export type AppDispatch = ReturnType<typeof store.dispatch>
+export type AppDispatch = typeof store.dispatch;
