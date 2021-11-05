abe9e790525743afb2c1fc9fe9b61318372efc8a
Zmiter <ozmiter@gmail.com>
2021-11-05 11:37:02 +0300

    doc: add todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React  from 'react';
 import './App.css';
 import {store} from "./store";
 import {Provider} from "react-redux";
@@ -6,8 +6,18 @@ import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
 import Navbar from "./components/Navbar";
 import {Layout} from "antd";
+// import {AuthActions} from "./store/reducers/auth/authActions";
+// import {UserInterface} from "./store/reducers/auth/authReducer";
 
 function App() {
+    //TODO make stay user authorised after reload page
+   // const dispatch = useDispatch()
+    // useEffect(()=>{
+    //     if(localStorage.getItem('isAuth')){
+    //         dispatch(AuthActions.setAuth(true))
+    //         dispatch(AuthActions.setUser({username: localStorage.getItem('username')|| '' } as UserInterface))
+    //     }
+    // }, [])
   return (
       <Provider store={store}>
           <BrowserRouter>
@@ -16,7 +26,6 @@ function App() {
                   <Layout.Content>
                       <AppRouter />
                   </Layout.Content>
-
               </Layout>
           </BrowserRouter>
       </Provider>
