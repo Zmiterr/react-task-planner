e7b324d9618ceb5cdc7be59010db6d781de476e7
Zmiter <ozmiter@gmail.com>
2021-11-10 02:48:24 +0300

    feat: add date validation

--- src/utils/rules.ts
+++ src/utils/rules.ts
@@ -1,6 +1,16 @@
+import moment, {Moment} from "moment";
+
 export const rules = {
     required: (message: string = 'Required') => ({
         required: true,
         message: message
+    }),
+    curDate: (message: string) => () => ({
+        validator(_: any, value: Moment) {
+            if(value.isSameOrAfter(moment())){
+                return Promise.resolve()
+            }
+            return Promise.reject(new Error(message))
+        }
     })
 }
